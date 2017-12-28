8dcf0ff3aa69564e9c923c0cfad6dc2f12c0e9fd
MathieuThiao <thiao-layel@et.esiea.fr>
2017-12-28 19:21:52 +0100

    Rating mechanic  [OK]

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `pfe`.article_list, `pfe`.users ;
+DROP TABLE IF EXISTS `pfe`.users ;
 
 CREATE TABLE `pfe`.`users` (
     `UserID` INT(25) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
@@ -8,13 +8,15 @@ CREATE TABLE `pfe`.`users` (
     `Categories` VARCHAR(65) NULL
 );
 
+DROP TABLE IF EXISTS `pfe`.article_list;
 CREATE TABLE `pfe`.`article_list` (
     `id` int(11) NOT NULL AUTO_INCREMENT UNIQUE,
     `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
     `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
     `date` DATE NOT NULL,
-    `rate` int(11),
+    `rate_historic` varchar(255),
     `number_rate` int(11),
+    `rate` int(11),
     `category` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
     `vignette_url` MEDIUMTEXT COLLATE utf8_unicode_ci,
     `resume` MEDIUMTEXT COLLATE utf8_unicode_ci,
@@ -23,19 +25,19 @@ CREATE TABLE `pfe`.`article_list` (
 );
 
 INSERT INTO `pfe`.article_list (`id`, `title`,`author`,`date`,`rate`,`number_rate`,`category`,`vignette_url`,`resume`,`content`)
-VALUES (NULL, 'fugiat','michel','2017-12-13',NULL ,NULL, 'Network', 'img/1.jpg','ah bah oui', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
+VALUES (NULL, 'fugiat','michel','2017-12-13',NULL ,NULL, 'animals', 'img/1.jpg','ah bah oui', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
 
 INSERT INTO `pfe`.article_list (`id`, `title`,`author`,`date`,`rate`,`number_rate`,`category`,`vignette_url`,`resume`,`content`)
-VALUES (NULL, 'parabout','jean-mi','2017-05-12',NULL ,NULL, 'Politics', 'img/2.jpg','ah bah non', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
+VALUES (NULL, 'parabout','jean-mi','2017-05-12',NULL ,NULL, 'computer-science', 'img/2.jpg','ah bah non', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
 
 INSERT INTO `pfe`.article_list (`id`, `title`,`author`,`date`,`rate`,`number_rate`,`category`,`vignette_url`,`resume`,`content`)
-VALUES (NULL, 'mounboots','clara','2017-09-02',NULL ,NULL, 'Network', 'img/3.jpg','ah bah peut etre', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
+VALUES (NULL, 'mounboots','clara','2017-09-02',NULL ,NULL, 'house', 'img/3.jpg','ah bah peut etre', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
 
 INSERT INTO `pfe`.article_list (`id`, `title`,`author`,`date`,`rate`,`number_rate`,`category`,`vignette_url`,`resume`,`content`)
-VALUES (NULL, 'jojolabricot','clara','2017-05-25',NULL ,NULL, 'Network', 'img/4.jpg','ah bah dsadsa', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
+VALUES (NULL, 'jojolabricot','clara','2017-05-25',NULL ,NULL, 'kids', 'img/4.jpg','ah bah dsadsa', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
 
 INSERT INTO `pfe`.article_list (`id`, `title`,`author`,`date`,`rate`,`number_rate`,`category`,`vignette_url`,`resume`,`content`)
-VALUES (NULL, 'loreneipsips','urabnfoot','2017-10-05',NULL ,NULL, 'Network', 'img/5.jpg','ah baaaaah oui', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
+VALUES (NULL, 'loreneipsips','urabnfoot','2017-10-05',NULL ,NULL, 'travels', 'img/5.jpg','ah baaaaah oui', '<div class="panel-footer">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.</div>');
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
--- rate.php
+++ rate.php
@@ -12,32 +12,30 @@ catch (Exception $e)
 
 $rate = (int)$_POST['rate'];
 $idArticle = $_POST['id'];
-/*echo "rate : ";
-echo $rate;
-echo ", id artcile :";
-echo $idArticle;*/
+
 $select = $bdd->query("SELECT * FROM article_list WHERE id = $idArticle LIMIT 1");
 $data = $select->fetch();
 
+$RatingList = explode(";",$data['rate_historic']);
+array_push($RatingList, $rate);
 
-$oldRate = (int)$data['rate'];
-$numberRate = (int)$data['number_rate'];
-$newRate = ((($oldRate*$numberRate) + $rate)/($numberRate + 1));
-
-echo $newRate;
-$newRate = (int)$newRate;
+$numberRate = $data['number_rate'];
 
 $new_numberRate = $numberRate + 1 ;
+$new_RatingList = implode(";",array_filter($RatingList));
 
-echo " ,";
-echo $newRate;
+$newRate = (array_sum($RatingList)/$new_numberRate);
 
-$bdd->query("UPDATE article_list SET rate = '".$newRate."' WHERE id =  $idArticle LIMIT 1");
-$bdd->query("UPDATE article_list SET number_rate = '".$new_numberRate."' WHERE id =  $idArticle LIMIT 1");
+$bdd->query("UPDATE article_list SET rate_historic = '".$new_RatingList."' WHERE id =  $idArticle");
+
+$bdd->query("UPDATE article_list SET number_rate = '".$new_numberRate."' WHERE id =  $idArticle");
+
+$bdd->query("UPDATE article_list SET rate = '".$newRate."' WHERE id =  $idArticle");
 
 ?>
 
-<!--<script>
+<script>
+    alert("THANKS FOR VOTING ;)");
 var clicked_id = <?php echo $idArticle; ?>;
 var datastr = 'id='+clicked_id;
 $("#main").load("article.php",{"id":clicked_id});
@@ -50,4 +48,4 @@ console.log("rate.php POST SUCCESS");
 }
 
 });
-</script>-->
+</script>
